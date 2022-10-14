68a09378d26f427af9b6503570e89c8194b96828
David Raluy <david@raluy.fr>
2022-10-15 00:56:55 +0200

    Moved table to custom element

---
+++ src/main/resources/static/customelements.js
@@ -0,0 +1,77 @@
+class UserTable extends HTMLElement{
+
+    static get observedAttributes() { return ['users']; }
+
+    constructor() {
+        super();
+        this._users = [];
+        this._table = {};
+    }
+
+    connectedCallback() {
+        let shadowRoot = this.attachShadow({ mode: "open" });
+
+        // Apply external styles to the shadow DOM
+        const linkElem = document.createElement("link");
+        linkElem.setAttribute("rel", "stylesheet");
+        linkElem.setAttribute("href", "https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/pure-min.css");
+
+// Attach the created element to the shadow DOM
+        shadowRoot.appendChild(linkElem);
+        this.table = document.createElement("table");
+        this.table.classList.add("pure-table")
+
+        let thead = document.createElement("thead");
+        thead.appendChild(this.createTh("id"))
+        thead.appendChild(this.createTh("First Name"))
+        thead.appendChild(this.createTh("Last Name"))
+        thead.appendChild(this.createTh("Email"))
+        thead.appendChild(this.createTh("Address"))
+        this.table.appendChild(thead)
+        this.table.appendChild(document.createElement("tbody"))
+        this.shadowRoot.appendChild(this.table)
+    }
+
+    get table(){
+        return this._table;
+    }
+
+    set table(tab){
+        this._table = tab;
+    }
+
+    get users() {
+        return this._users;
+    }
+
+    set users(usersPassed) {
+        this._users = [...usersPassed]
+
+        let tbody = document.createElement("tbody");
+        for(const user of this._users){
+            let tr = document.createElement("tr");
+            tbody.appendChild(tr);
+            this.createCell(user, tr, "id");
+            this.createCell(user, tr, "first_name");
+            this.createCell(user, tr, "last_name");
+            this.createCell(user, tr, "email");
+            this.createCell(user, tr, "address");
+        }
+        this.table.removeChild(this.table.childNodes[1]);
+        this.table.append(tbody);
+    }
+
+    createTh(text) {
+        let thead = document.createElement("th");
+        thead.innerText = text;
+        return thead;
+    }
+
+    createCell(user, tr, prop) {
+        let cell = document.createElement("td");
+        cell.innerHTML = user[prop]
+        tr.appendChild(cell);
+    }
+}
+
+customElements.define("user-table", UserTable, {});
